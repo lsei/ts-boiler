d26a606f357ea00a8a29c9cf0c7663ec171165dd
Lucas <lsei@users.noreply.github.com>
2021-01-09 12:18:46 +0000

    basic working setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = [
+    {
+        mode: 'development',
+        entry: './src/index.ts',
+        target: 'es5',
+        devtool: 'eval',
+        resolve: {
+            extensions: ['.js', '.ts'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    include: /src/,
+                    use: [{ loader: 'ts-loader' }],
+                },
+                {
+                    test: /\.(glsl|vert|frag)$/,
+                    use: 'raw-loader',
+                },
+            ],
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'static'),
+            port: 4000,
+            // publicPath: '/build',
+        },
+        output: {
+            path: __dirname + '/build',
+            filename: 'bundle.js',
+        },
+    },
+];
